dc4c85fcf4771f0ac199e2ca6fa8bb6491e6eb4a
Karsten <karstenseijkens@gmail.com>
2024-10-18 10:33:50 +0200

    added notes for what needs to be done in the login controller

--- back-end-geoprofs/app/Http/Controllers/log_in_controller.php
+++ back-end-geoprofs/app/Http/Controllers/log_in_controller.php
@@ -23,9 +23,16 @@ class log_in_controller extends Controller
             $user = User::where('id', $request->emailOrId)->first();//depends on model
         }
 
+        //log in attempts limiter need to be added
+
         if(is_null($user) || Hash::check($request->password, $user->password)){
+
+            //error message for return needs to be added
+
             return(Null);//if login was not successful return stops code from continuing
         }
+
+        //authentication and session needs to be added
 
         return(Null);//if login was successful
     }
