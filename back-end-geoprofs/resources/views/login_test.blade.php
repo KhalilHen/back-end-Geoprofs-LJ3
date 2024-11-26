50b2e55cd41017d3da4bb8f9f25f531ee58f6a79
Karsten <karstenseijkens@gmail.com>
2024-11-26 11:06:46 +0100

    changed login  to try to fix it

---
+++ back-end-geoprofs/resources/views/login_test.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
+        <label for="emailOrId">Email or ID:</label>
+        <input type="text" id="emailOrId" name="emailOrId" required>
+        <br><br>
+
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required>
+        <br><br>
+
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
