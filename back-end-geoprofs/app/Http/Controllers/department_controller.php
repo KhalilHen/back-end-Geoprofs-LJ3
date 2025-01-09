87615a7b1a21684db45452b7c8e10ff54683f5d5
Karsten <karstenseijkens@gmail.com>
2025-01-09 11:50:47 +0100

    added check for section manger

--- back-end-geoprofs/app/Http/Controllers/department_controller.php
+++ back-end-geoprofs/app/Http/Controllers/department_controller.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\DepartmentEmployee;
+use App\Models\DepartmentSection;
+use App\Models\Section;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
@@ -12,7 +14,6 @@ use Illuminate\Support\Facades\Log;
 class department_controller extends Controller
 {
     public function getUsers(Request $request){
-        Log::info('getUsers endpoint hit.');
 
         $request->validate([
             'idUser' => 'required|integer',
@@ -20,6 +21,8 @@ class department_controller extends Controller
             'cacheId' => 'required|string',
             'idDepartment' => 'required|integer'
         ]);
+
+        Log::info('getUsers from department controller');
 
         $userToken = Cache::get('user_token:' . $request->idUser . "_" . $request->cacheId);
 
@@ -30,7 +33,12 @@ class department_controller extends Controller
         $user = User::where('id', $request->idUser)->first();
         $departmentEmployee = DepartmentEmployee::where('user_id', $request->idUser)->first();
 
-        if(!($user->role == 'CEO' || $departmentEmployee->department_id == $request->idDepartment || ($user->role == 'section-Manager' && true /* to do: check if section manger is managing has target department */))){
+        if(!(
+            $user->role == 'CEO' ||
+            $departmentEmployee?->department_id == $request->idDepartment ||
+            ($user->role == 'section-Manager' && $this->sectionMangerCheck($request->idUser , $request->idDepartment))
+        ))
+        {
             return response()->json(['message' => 'You do not have the necessary permissions to access this data.'], 403);
         }
 
@@ -41,5 +49,17 @@ class department_controller extends Controller
         return response()->json([
             'user_ids' => $userIds
         ]);  
+    }
+
+    private function sectionMangerCheck($userId , $departmentId)
+    {
+        Log::info('sectionMangerCheck from department controller');
+        $section = Section::where('manager_role_id' , $userId)->first();
+        Log::info('id of section is:' . $section?->id);
+        $departmentSection = DepartmentSection::where('department_id' , $departmentId)->first();
+        if($departmentSection?->section_id == $section?->id){
+            return true;
+        }
+        return false;
     }
 }
