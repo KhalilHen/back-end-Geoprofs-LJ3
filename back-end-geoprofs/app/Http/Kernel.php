96d46a1b0df0a3925355685091e5e7b285d9d0a5
Karsten <karstenseijkens@gmail.com>
2025-01-15 21:32:10 +0100

    turned of cors to make develoment easier

--- back-end-geoprofs/app/Http/Kernel.php
+++ back-end-geoprofs/app/Http/Kernel.php
@@ -13,6 +13,8 @@ class Kernel extends HttpKernel
      *
      * @var array<int, class-string|string>
      */
+
+    //cors removed for easier development
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
@@ -22,7 +24,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \Illuminate\Http\Middleware\HandleCors::class,
-        \App\Http\Middleware\Cors::class,
+        // \App\Http\Middleware\Cors::class,
     ];
 
     /**
