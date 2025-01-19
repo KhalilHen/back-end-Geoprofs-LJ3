b7e440a0911159fd1eebd5226a68e1dced8e415b
Karsten <karstenseijkens@gmail.com>
2025-01-19 19:53:51 +0100

    create seeder for testing

--- back-end-geoprofs/database/seeders/TestDatabaseSeeder.php
+++ back-end-geoprofs/database/seeders/TestDatabaseSeeder.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use App\Models\Section;
+use App\Models\DepartmentEmployee;
+use App\Models\DepartmentSection;
+use App\Models\Project;
+use App\Models\ProjectEmployee;
 
 class TestDatabaseSeeder extends Seeder
 {
@@ -16,49 +20,36 @@ class TestDatabaseSeeder extends Seeder
     public function run(): void
     {
         //clear data base via "php artisan migrate:fresh" first so ids have no conflicts if you don't want to do this with you current data base change database in the .env file
-        $employees = [];
-        $userId = 1;
 
-        for($i = 1; $i <= 36; $i++) {
-            array_push($employees, [
-                'id' => $userId,
-                'name' => 'TestUser' . $i,
-                'email' => 'GeoprofsTestUser' . $i . '@example.com',
-                'password' => 'password' . $userId,
-                'role' => 'employee',
-            ]);
-            $userId++;
-        }
-
-        $departmentMangers = [];
-        $departmentCount = 7;
-
-        for($i = 1; $i <= $departmentCount; $i++) {
-            array_push($departmentMangers, [
-                'id' => $userId,
-                'name' => 'TestDepartmentManger' . $i,
-                'email' => 'GeoprofsDepartmentManger' . $i . '@example.com',
-                'password' => 'password' . $userId,
-                'role' => 'manager',
-            ]);
-            $userId++;
-        }
-
-        $departments = [];
-
-        for($i = 1; $i <= $departmentCount; $i++) {
-            array_push($departments, [
-                'id' => $i,
-                'title' => 'TestDepartment' . $i,
-                'manager_role_id' => $departmentMangers[$i-1]['id'],
-                'description' => ' this is TestDepartment' . $i,
-            ]);
-        }
-
+        $employeesStructure = [
+            [
+                4,
+                5,
+                6,
+            ],
+            [
+                5,
+                6,
+            ],
+            [
+                5,
+                6,
+            ],
+        ];
+        
+        $userId = 0;
+        $employee = 0;
+        $departmentId = 0;
         $sectionMangers = [];
-        $sectionCount = 3;
+        $sections = [];
+        $departmentMangers = [];
+        $departments = [];
+        $employees = [];
+        $departmentEmployees = [];
+        $departmentSections = [];
 
-        for($i = 1; $i <= $sectionCount; $i++) {
+        for($i = 1; $i <= sizeof($employeesStructure); $i++) {
+            $userId++;
             array_push($sectionMangers, [
                 'id' => $userId,
                 'name' => 'TestSectionManger' . $i,
@@ -66,20 +57,63 @@ class TestDatabaseSeeder extends Seeder
                 'password' => 'password' . $userId,
                 'role' => 'section-Manager',
             ]);
-            $userId++;
-        }
 
-        $sections = [];
-
-        for($i = 1; $i <= $sectionCount; $i++) {
             array_push($sections, [
                 'id' => $i,
                 'title' => 'TestSection' . $i,
                 'manager_role_id' => $sectionMangers[$i-1]['id'],
                 'description' => ' this is TestSection' . $i,
             ]);
+
+            for($j = 1; $j <= sizeof($employeesStructure[$i-1]); $j++) {
+                
+                $userId++;
+                $departmentId++;
+                array_push($departmentMangers, [
+                    'id' => $userId,
+                    'name' => 'TestDepartmentManger' . $departmentId,
+                    'email' => 'GeoprofsDepartmentManger' . $departmentId . '@example.com',
+                    'password' => 'password' . $userId,
+                    'role' => 'manager',
+                ]);
+
+                array_push($departments , [
+                    'id' => $departmentId,
+                    'title' => 'TestDepartment' . $departmentId,
+                    'manager_role_id' => $departmentMangers[$departmentId-1]['id'],
+                    'description' => ' this is TestDepartment' . $departmentId,
+                ]);
+
+                array_push($departmentEmployees, [
+                    'department_id' => $departmentId,
+                    'user_id' => $departmentMangers[$departmentId-1]['id'],
+                ]);
+
+                array_push($departmentSections, [
+                    'department_id' => $departmentId,
+                    'section_id' => $i,
+                ]);
+
+                for($k = 1; $k <= $employeesStructure[$i-1][$j-1]; $k++){
+                    $userId++;
+                    $employee++;
+                    array_push($employees, [
+                        'id' => $userId,
+                        'name' => 'TestEmployee' . $employee,
+                        'email' => 'GeoprofsEmployee' . $employee . '@example.com',
+                        'password' => 'password' . $userId,
+                        'role' => 'employee',
+                    ]);
+
+                    array_push($departmentEmployees, [
+                        'department_id' => $departmentId,
+                        'user_id' => $employees[$employee-1]['id'],
+                    ]);
+                }
+            }
         }
 
+        $userId++;
         $ceo = [
             'id' => $userId,
             'name' => 'CEO',
@@ -87,7 +121,43 @@ class TestDatabaseSeeder extends Seeder
             'password' => 'password' . $userId,
             'role' => 'CEO',
         ];
-        $userId++;
+        
+        $projectsSetup = [
+            [
+                1,
+                2,
+                5,
+                6,
+                16,
+                17,
+            ],
+            [
+                16,
+                17,
+                32,
+                33,
+                34,
+            ],
+        ];
+
+        $projects = [];
+        $projectsEmployees = [];
+
+        for ($i = 1; $i <= sizeof($projectsSetup); $i++){
+            array_push($projects , [
+                'id' => $i,
+                'title' => 'Project' . $i,
+                'description' => 'This is project ' . $i,
+            ]);
+
+            for ($j = 1; $j <= sizeof($projectsSetup[$i-1]); $j++){
+                array_push($projectsEmployees, [
+                    'project_id' => $i,
+                    'user_id' => $employees[$projectsSetup[$i-1][$j-1]-1]['id'],
+                ]);
+            }
+        }
+        
 
         $users = array_merge($employees, $departmentMangers, $sectionMangers , [$ceo]);
 
@@ -99,8 +169,24 @@ class TestDatabaseSeeder extends Seeder
             Department::create($department);
         }
 
+        foreach ($departmentEmployees as $employee) {
+            DepartmentEmployee::create($employee);
+        }
+
         foreach($sections as $section) {
             Section::create($section);
+        }
+
+        foreach($departmentSections as $departmentSection) {
+            DepartmentSection::create($departmentSection);
+        }
+
+        foreach($projects as $project) {
+            Project::create($project);
+        }
+
+        foreach($projectsEmployees as $projectEmployee) {
+            ProjectEmployee::create($projectEmployee);
         }
     }
 }
